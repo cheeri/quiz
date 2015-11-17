70850be0790044e4b5a7d1406b44e7bbcea3d183
murali.ambekar@gmail.com <murali.ambekar@gmail.com>
2015-11-16 19:58:00 -0600

    Temp

--- js/app.js
+++ js/app.js
@@ -1,8 +1,34 @@
 var manager = quizManager();
 
+var quiz = {
+	questions: [],
+	currentquestion:0,
+	ask:function(){
+		$('h2').text(this.questions[this.currentquestion].ask);
+		$('#label1').text(this.questions[this.currentquestion].choices[0]);
+		$('li#choice2 label').text(this.questions[this.currentquestion].choices[1]);
+		$('li#choice3 label').text(this.questions[this.currentquestion].choices[2]);
+		$('li#choice4 label').text(this.questions[this.currentquestion].choices[3]);
+
+	}
+
+
+}
+
+var question = {
+	ask      :'Who won the world cup in 1983', 
+	choices  :['India','Australia','West Indies','England'],
+	answer   :'India'
+}
+
 $( document ).ready(function() {
 
+	$('li').click(function(e){
+    	e.stopPropagation();
+  	});
+
 	manager.initializeData();
+
 
 });
 
@@ -31,46 +57,30 @@ function quizManager(){
 	}
 
 	function initializeData(){
-		wcquestion1 = Object.create(question);
-		wcquestion1.question = 'Who won the 1987 World Cup';
-		wcquestion1.choices = ['India','Australia','West Indies','England'];
+		
+		var quiz1 = Object.create(quiz);
+
+		var wcquestion1 = Object.create(question);
+		wcquestion1.ask = 'Who won the 1987 World Cup';
+		wcquestion1.choices = ['NewZealand','Australia','West Indies','England'];
 		wcquestion1.answer = 'Australia';
 
-		wcquestion2 = Object.create(question);
+		var wcquestion2 = Object.create(question);;
 		wcquestion2.question = 'Who won the 1992 World Cup';
-		wcquestion2.choices = ['India','Australia','Pakistan','England'];
+		wcquestion2.choices = ['India','West Indies','Pakistan','Sri Lanka'];
 		wcquestion2.answer = 'Pakistan';
 
-		quiz1 = Object.create(quiz);
 		quiz1.questions=[wcquestion1,wcquestion2];
 		listOfQuiz = [quiz1];
 
 		//Ask the first question
-		quiz1.askQuestion();
+		quiz1.ask();
 
 	}
 
-	return{handleResponse:handleResponse, initializeData:initializeData }
+	return{handleResponse:handleResponse, initializeData:initializeData };
 }
 
 
-function quiz(){
-	var questions;
-	var currentQuestionIndex = 0;
-	function handleResponse(){
 
-	}
 
-	function askQuestion(){
-		$('h2').html(questions[currentQuestionIndex].question);
-
-	}
-
-	return {questions:questions,handleResponse:handleResponse, askQuestion:askQuestion}
-}
-
-function question(){
-	question :'Who won the world cup in 1983';
-    choices  :['India','Australia','West Indies','England'];
-	answer   :'India';
-}
